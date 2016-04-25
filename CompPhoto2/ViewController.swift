ac9bb488ab57b380a35ba1b1100fed912e003913
Stewart Boyd <stewartboyd1988@gmail.com>
2016-04-25 00:11:03 -0400

    Initial Commit

---
+++ CompPhoto2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CompPhoto2
+//
+//  Created by Stewart Boyd on 4/25/16.
+//  Copyright © 2016 Stewart Boyd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
